6bdd8c1ca07acd9d4852af4aee7064157e67935e
Star <mmdakane@rocketmail.com>
2026-04-06 06:15:23 +0200

    moved assets

--- src/components/ModalList.jsx
+++ src/components/ModalList.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ModalListItems from './ModalListItems'
 
 const ModalList = () => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("../../data.json");
+                const data = await res.json();
+                setData(data);
+                console.log(data);
+            } catch (error) {
+                console.log('Error fetching data', error)
+            }
+        }
+
+        fetchData();
+    }, [])
+
     return (
         <ul className='flex flex-col gap-4'>
-            <ModalListItems />
+            {/* Make an array to add items */}
+            <ModalListItems data={data} />
         </ul>
     )
 }

56ea3edb171f11a1e5cbc8297b5b3b94aabdc6ad
Star <mmdakane@rocketmail.com>
2026-04-06 21:38:33 +0200

    cart empty toggle

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import EmptyCart from './EmptyCart'
 import LoadedCart from './LoadedCart'
+import { GlobalContext } from '../contexts/GlobalContext'
 
 const Cart = ({ setOpenModal }) => {
+
+    const { list } = useContext(GlobalContext);
+
+
+
     return (
         <section className='bg-white p-6 flex flex-col gap-6 col-start-1 col-end-3 w-81.7 lg:col-start-3 lg:w-[384px] rounded-xl'>
-            {/* <EmptyCart /> */}
-            <LoadedCart setOpenModal={setOpenModal} />
+            {list.length === 0 ? <EmptyCart /> : <LoadedCart setOpenModal={setOpenModal} />}
         </section>
     )
 }

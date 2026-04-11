384502a4bc14e13bde1130ed9dfd2e6504b0aae8
Star-Mdakane <101474151+Star-Mdakane@users.noreply.github.com>
2026-04-11 18:06:39 +0200

    Update LoadedCart.jsx

--- src/components/LoadedCart.jsx
+++ src/components/LoadedCart.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-import CartListItems from './CartListItems'
 import CartList from './CartList'
 import { GlobalContext } from '../contexts/GlobalContext'
 
